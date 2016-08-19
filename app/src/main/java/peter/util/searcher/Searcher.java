a3ae47a660e6e306bd362c20f3ff17ed5156f439
peter <javalive09@163.com>
2016-08-19 17:00:17 +0800

    add voice module

--- app/src/main/java/peter/util/searcher/Searcher.java
+++ app/src/main/java/peter/util/searcher/Searcher.java
@@ -2,7 +2,8 @@ package peter.util.searcher;
 
 import android.app.Application;
 import peter.util.searcher.net.RequestManager;
-
+import com.iflytek.cloud.SpeechConstant;
+import com.iflytek.cloud.SpeechUtility;
 /**
  * Created by peter on 16/5/19.
  */
@@ -12,6 +13,7 @@ public class Searcher extends Application {
     public void onCreate() {
         super.onCreate();
         RequestManager.init(this);
+        SpeechUtility.createUtility(this, SpeechConstant.APPID + "=5775fcb4");
     }
 
 }
--- app/src/main/java/peter/util/searcher/activity/EnterActivity.java
+++ app/src/main/java/peter/util/searcher/activity/EnterActivity.java
@@ -6,6 +6,7 @@ import android.app.FragmentTransaction;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Configuration;
+import android.graphics.drawable.LevelListDrawable;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -21,7 +22,21 @@ import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.iflytek.cloud.ErrorCode;
+import com.iflytek.cloud.InitListener;
+import com.iflytek.cloud.SpeechConstant;
+import com.iflytek.cloud.SpeechError;
+import com.iflytek.cloud.SpeechRecognizer;
+import com.iflytek.cloud.ui.RecognizerDialog;
+import com.iflytek.cloud.ui.RecognizerDialogListener;
 import com.umeng.analytics.MobclickAgent;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
 import java.util.ArrayList;
 import peter.util.searcher.update.AsynWindowHandler;
 import peter.util.searcher.engine.EngineViewPagerFragment;
@@ -45,6 +60,11 @@ public class EnterActivity extends BaseActivity implements DrawerLayoutAdapter.O
     private static final String HISTORY_TODAY_URL = "http://wap.lssdjt.com/";
     private static final String NEWS_URL = "http://3g.163.com/touch/news";
     private static final String NAV_URL = "http://3g.hao123.com/";
+
+    // 语音听写对象
+    private SpeechRecognizer mIat;
+    // 语音听写UI
+    private RecognizerDialog mIatDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -76,6 +96,11 @@ public class EnterActivity extends BaseActivity implements DrawerLayoutAdapter.O
     }
 
     private void init() {
+        mIat = SpeechRecognizer.createRecognizer(EnterActivity.this, mInitListener);
+        mIatDialog = new RecognizerDialog(EnterActivity.this, mInitListener);
+        mIatDialog.setListener(mRecognizerDialogListener);
+        setParam();
+
         opt = (ImageView) findViewById(R.id.opt);
         search = (EditText) findViewById(R.id.search);
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -132,10 +157,10 @@ public class EnterActivity extends BaseActivity implements DrawerLayoutAdapter.O
                 String content = s.toString();
                 if (TextUtils.isEmpty(content)) {
                     setEngineFragment(RECENT_SEARCH);
-                    opt.setVisibility(View.GONE);
+                    opt.getDrawable().setLevel(0);
                 } else if (!content.equals(temp)) {
                     setEngineFragment(ENGINE_LIST);
-                    opt.setVisibility(View.VISIBLE);
+                    opt.getDrawable().setLevel(1);
                 }
             }
         });
@@ -255,11 +280,96 @@ public class EnterActivity extends BaseActivity implements DrawerLayoutAdapter.O
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.opt:
-                search.requestFocus();
-                search.setText("");
-                openBoard();
+
+                switch (opt.getDrawable().getLevel()) {
+                    case 0:
+                        mIatDialog.show();
+                        break;
+                    case 1:
+                        search.requestFocus();
+                        search.setText("");
+                        openBoard();
+                        break;
+                }
                 break;
         }
     }
 
+    //Listener for dialog
+    private InitListener mInitListener = new InitListener() {
+
+        @Override
+        public void onInit(int code) {
+            if (code != ErrorCode.SUCCESS) {
+                Toast.makeText(EnterActivity.this, "error code " + code, Toast.LENGTH_SHORT).show();
+            }
+        }
+    };
+
+
+    private void setParam() {
+        mIat.setParameter(SpeechConstant.PARAMS, null);
+
+        // 设置听写引擎
+        mIat.setParameter(SpeechConstant.ENGINE_TYPE, SpeechConstant.TYPE_CLOUD);
+        // 设置返回结果格式
+        mIat.setParameter(SpeechConstant.RESULT_TYPE, "json");
+
+        // 设置语言
+        mIat.setParameter(SpeechConstant.LANGUAGE, "zh_cn");
+        // 设置语言区域
+        mIat.setParameter(SpeechConstant.ACCENT, "mandarin");
+
+        // 设置语音前端点:静音超时时间，即用户多长时间不说话则当做超时处理
+        mIat.setParameter(SpeechConstant.VAD_BOS, "4000");
+
+        // 设置语音后端点:后端点静音检测时间，即用户停止说话多长时间内即认为不再输入， 自动停止录音
+        mIat.setParameter(SpeechConstant.VAD_EOS, "1000");
+
+        // 设置标点符号,设置为"0"返回结果无标点,设置为"1"返回结果有标点
+        mIat.setParameter(SpeechConstant.ASR_PTT, "0");
+
+    }
+    /**
+     * Dialog监听器
+     */
+    private RecognizerDialogListener mRecognizerDialogListener = new RecognizerDialogListener() {
+        @Override
+        public void onResult(com.iflytek.cloud.RecognizerResult recognizerResult, boolean isLast) {
+
+            if (recognizerResult != null) {
+                String json = recognizerResult.getResultString();
+                if (!TextUtils.isEmpty(json)) {
+                    String result = parseIatResult(json);
+                    if(!TextUtils.isEmpty(result)) {
+                        setSearchWord(result);
+                    }
+                }
+            }
+        }
+
+        @Override
+        public void onError(SpeechError speechError) {
+
+        }
+    };
+
+    private String parseIatResult(String json) {
+        StringBuffer ret = new StringBuffer();
+        try {
+            JSONTokener tokener = new JSONTokener(json);
+            JSONObject joResult = new JSONObject(tokener);
+
+            JSONArray words = joResult.getJSONArray("ws");
+            for (int i = 0; i < words.length(); i++) {
+                // 转写结果词，默认使用第一个结果
+                JSONArray items = words.getJSONObject(i).getJSONArray("cw");
+                JSONObject obj = items.getJSONObject(0);
+                ret.append(obj.getString("w"));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return ret.toString();
+    }
 }
