078dd2f48791d66d12bd19cae718d5a67b127a9c
peter <peter@peterdeMacBook-Pro.local>
2016-12-07 13:13:15 +0800

    fix stoploading bug

--- app/src/main/java/peter/util/searcher/activity/MainActivity.java
+++ app/src/main/java/peter/util/searcher/activity/MainActivity.java
@@ -338,6 +338,7 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
     public void refreshGoForward(boolean isActivate) {
         if(isActivate) {
             findViewById(R.id.go_forward).setActivated(true);
+            findViewById(R.id.go_forward).setEnabled(true);
         }else {
             findViewById(R.id.go_forward).setActivated(false);
             if (manager.getCurrentTabGroup().canGoForward()) {
